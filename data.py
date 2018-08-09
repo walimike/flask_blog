4cfb3ee5a3687eced2c5975df1c0768f24bea0e3
walimike <walimike139@gmail.com>
2018-08-09 14:36:05 +0300

    First Commit

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+
+        {
+            'id': 1,
+            'title': 'Article one',
+            'body': 'blah blah blah',
+            'author': 'Audrey Karungi',
+            'Created_date': '18-07-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'blah blah blah',
+            'author': 'Joseph Mutumi',
+            'Created_date': '18-07-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'blah blah blah',
+            'author ': 'Michael Wali',
+            'Created_date': '18-07-2018'
+        }
+
+    ]
+    return articles
